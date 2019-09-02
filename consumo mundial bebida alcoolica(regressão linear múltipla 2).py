087e12c384ebd93ad70d47d895480c4cf7168a89
fernandessfae <fernandessfae@hotmail.com>
2019-09-02 16:46:48 -0300

    Alguns modelos de Data Science
    Aqui estou fazendo o upload de modelos de regressão linear simples e múltipla através do site https://fivethirtyeight.com/features/dear-mona-followup-where-do-people-drink-the-most-beer-wine-and-spirits/ que mostra a quantidade de álcool que cada pessoa consome através das bebidas alcoólicas tradicionais. Para mais informações, basta abrir os códigos que estarão totalmente explicados, passo-a-passo.

---
+++ consumo mundial bebida alcoolica(regressão linear múltipla 2).py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import statsmodels.formula.api as sm
+
+''' OBS¹: Dos 193 países selecionados,apenas 180 foram selecionados (13 países abstêmicos foram excluidos)
+ em todas as bebidas alcoólicas '''
+'''OBS²: O consumo é per capita '''
+
+bebida_mundo = pd.read_csv('D:\\Meus Documentos\Desktop\\Projetos Cientista de Dados\\Consumo bebidas alcóolicas no mundo\\Consumo de bebidas alcoólicas ao redor do mundo.csv')
+bebida_mundo = bebida_mundo.drop([0, 13, 46, 79, 90, 97, 103, 106, 107, 111, 128, 147, 158])
+
+#Seleciona as colunas para serem as variáveis explanatória e resposta(X e y, respectivamente)
+X = bebida_mundo.iloc[:, 1:4] #beer_servings, spirit_servings e wine_servings
+y = bebida_mundo.iloc[:, 4] #total_litres_of_alcohol
+
+modelo = LinearRegression()
+modelo.fit(X, y)
+
+# A função score serve para mostrar o grau de infuencia das variáveis explicativas sobre a variável de resposta.
+modelo.score(X, y)
+
+# As três linhas de comando a seguir tem a função de mostrar todos os valores detalhados, parecido no R
+modelo_ajustado = sm.ols(formula = 'total_litres_of_pure_alcohol ~ beer_servings + spirit_servings + wine_servings',
+                         data = bebida_mundo)
+modelo_treinado = modelo_ajustado.fit()
+modelo_treinado.summary()
+
+'''Na variável teste, será designado valores para uma nova previsao qualquer,
+ atentando o fato de se colocar os valores na ordem das variáveis passadas anteriormente.'''
+ 
+teste = np.array([400, 400, 400])
+'''Ex:beer_servings = 400 copos
+   spirit_servings = 400 copos
+   wine_servings = 400 taças '''
+teste = teste.reshape(1, -1)
+modelo.predict(teste)
+
+'''OBS³:  Nesse caso foi adotado que o volume de um copo de cerveja, destilado e vinho são de 300 ml (0.3 L), 100 ml (0.1 L)
+e 450 ml (0.45 L), este que usará apenas 150 ml (0.15 L) por taça. Então supondo que uma pessoa consuma 400 copos/taças de cada
+bebida, pode dizer que está consumindo 120 L, 40 L e 60 L de cerveja, destilados e vinho, respectivamente. No total, consumiu
+cerca de 220 L de bebida alcoólica, dos quais, aproximadamente, 20.45 L são de puro álcool, correspondendo cerca de 9.3 % de 
+álcool, aproximadamente.'''
+
+'''Obs: O matplotlib poderia ter sido usado, entretando com há mais de uma váriavel,
+teriamos de importar um modelo que suporte mais de 2 dimensões, sem contar no poder
+de processamento que seria necessário para gerar o gráfico. '''
---
+++ consumo mundial bebida alcoolica(regressão linear simples 2).py
@@ -0,0 +1,124 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from yellowbrick.regressor import ResidualsPlot
+
+''' OBS¹: Dos 193 países selecionados,apenas 180 foram selecionados (13 países abstêmicos foram excluidos)
+ em todas as bebidas alcoólicas '''
+'''OBS²: O consumo é per capita '''
+
+bebida_mundo = pd.read_csv('D:\\Meus Documentos\Desktop\\Projetos Cientista de Dados\\Consumo bebidas alcóolicas no mundo\\Consumo de bebidas alcoólicas ao redor do mundo.csv')
+bebida_mundo = bebida_mundo.drop([0, 13, 46, 79, 90, 97, 103, 106, 107, 111, 128, 147, 158])
+
+'''1) Relação linear com o total de alcool ingerido (em Litros) com o total de cerveja ingerida (em porções) '''
+
+#Seleciona as colunas para serem as variáveis explanatória e resposta(X e y, respectivamente)
+X = bebida_mundo.iloc[:, 1].values #beer_servings
+y = bebida_mundo.iloc[:, 4].values #total_litres_of_alcohol
+correlacao = np.corrcoef(X, y)
+
+#A função reshape é necessaria para transformar o X em matriz, para assim ser colocada no FIT
+#Lembrando que com a transformaçao de X em matriz, só será possível executar a correlação no seu formato original
+X = X.reshape(-1, 1)
+modelo1 = LinearRegression()
+modelo1.fit(X, y)
+
+#Mostra a interceptação e inclinação do modelo(intercept e coef, respectivamente)
+modelo1.intercept_
+modelo1.coef_
+
+#Plotar o gráfico com a linha de regressão é necessário executar os 4 comandos plt juntos
+plt.scatter(X, y)
+plt.xlabel('Copos de cerveja (por ano)')
+plt.ylabel('Total de litros de álcool ingeridos (por ano)')
+plt.plot(X, modelo1.predict(X), color = 'red')
+
+#Calculo manual com o modelo treinado para qualquer achar o y (x[beer_servings] = 400)
+modelo1.intercept_ + modelo1.coef_ * 400
+
+#Cálculo automático da máquina
+modelo1.predict([[400]])
+
+''' Como no estudo não informou uma porção em litros como referência, podemos fazer suposições a partir desse modelo
+por exemplo, se uma pessoa bebe 400 copos de cerveja por ano, e adotando um copo de cerveja com 300 ml (0,3L), uma
+pessoa que bebe 400 copos por ano (dependendo de cada país, obviamente), bebe 120 litros de cerveja, e só de alcool puro,
+uma pessoa bebe, aproximadamente, 13.65 litros de álcool (cerca de 11.37% aproximadamente) '''
+
+#Visualização dos resíduos(resultado entre a distância dos pontos com a linha de referência)
+modelo1._residues
+
+#Visualização dos resíduos no gráfico
+visualizador1 = ResidualsPlot(modelo1)
+visualizador1.fit(X, y)
+visualizador1.poof()
+
+#Os resíduos quando mais próximo de zero, melhor o modelo
+
+'''2) Relação linear entre total de álcool ingerido (em Litros) com o total de destilados ingerido (em porções)
+   OBS: Bebidas destiladas são todas que tiveram seu processo de destilação (vodca, uísque, tequila, rum, dentre outros)  '''
+
+A = bebida_mundo.iloc[:, 2].values #spirit_servings 
+b = bebida_mundo.iloc[:, 4].values #total_litres_of_alcohol
+correlacao2 = np.corrcoef(A, b)
+
+A = A.reshape(-1, 1)
+modelo2 = LinearRegression()
+modelo2.fit(A, b)
+
+modelo2.intercept_
+modelo2.coef_
+
+plt.scatter(A, b)
+plt.xlabel('Copos de destilados por pessoa (por ano)')
+plt.ylabel('Total de litros de álcool ingeridos (por ano)')
+plt.plot(A, modelo2.predict(A), color = 'yellow')
+
+modelo2.intercept_ + modelo2.coef_ * 400
+
+modelo2.predict([[400]])
+
+''' Como no estudo não informou uma porção em litros como referência, podemos fazer suposições a partir desse modelo
+por exemplo, se uma pessoa bebe 400 copos de destilados por ano, e adotando um copo de destilado com 100 ml (0,1L), uma
+pessoa que bebe 400 copos por ano (dependendo de cada país, obviamente), bebe 40 litros de destilado, e só de alcool puro,
+uma pessoa bebe, aproximadamente, 13.21 litros de álcool (cerca de 33.2% aproximadamente) '''
+
+modelo2._residues
+
+visualizador2 = ResidualsPlot(modelo2)
+visualizador2.fit(X, y)
+visualizador2.poof() 
+
+'''3) Relação linear entre total de álcool ingerido (em Litros) em relação com o total de vinho ingerido (em porções) ''' 
+
+C = bebida_mundo.iloc[:, 3].values #wine_servings 
+d = bebida_mundo.iloc[:, 4].values #total_litres_of_alcohol
+correlacao3 = np.corrcoef(C, d)
+
+C = C.reshape(-1, 1)
+modelo3 = LinearRegression()
+modelo3.fit(C, d)
+
+modelo3.intercept_
+modelo3.coef_
+
+plt.scatter(C, d)
+plt.xlabel('Taças de vinho por pessoa (por ano)')
+plt.ylabel('Total de litros de álcool ingeridos (por ano)')
+plt.plot(C, modelo3.predict(C), color = 'purple')
+
+modelo3.intercept_ + modelo3.coef_ * 400
+
+modelo3.predict([[400]]) 
+
+''' Como no estudo não informou uma porção em litros como referência, podemos fazer suposições a partir desse modelo
+por exemplo, se uma pessoa bebe 400 copos de destilados por ano, e adotando uma taça de de vinho com 450 ml (0,45L),
+entretanto uma pessoa só bebe cerca de 1/3 da taça então uma pessoa bebe 150 ml (0,15L), uma pessoa que bebe 400 copos
+por ano (dependendo de cada país, obviamente), bebe 60 litros de vinho, e só de alcool puro,
+uma pessoa bebe, aproximadamente, 15.38 litros de álcool (cerca de 25.63% aproximadamente) '''
+
+modelo3._residues
+
+visualizador3 = ResidualsPlot(modelo3)
+visualizador3.fit(X, y)
+visualizador3.poof()
