c44eb3ef269f6b7b657090fad362cfcb3cbe1370
Aristóteles Fernandes <fernandessfae@hotmail.com>
2019-09-29 17:39:04 -0300

    Update consumo mundial bebida alcoolica(classificação com Árvore de decisão).py

--- consumo mundial bebida alcoolica(classificação com Árvore de decisão).py
+++ consumo mundial bebida alcoolica(classificação com Árvore de decisão).py
@@ -3,32 +3,27 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_graphviz
+from yellowbrick.classifier import ConfusionMatrix
 
 '''Aqui ulitizaremos o método de classificação com a Árvore de decisão, para ver se eles acertam a bebida marjoritária de cada país. '''
 
 #Carregamento da base de dados
 bebida_mundo = pd.read_csv('D:\\Meus Documentos\\Desktop\\Projetos Cientista de Dados\\Consumo bebidas alcóolicas no mundo\\Consumo de bebidas alcoólicas ao redor do mundo.csv')
 
-'''Atenção¹: nessa nova coluna atribuirei valores numéricos/nominais para cada categoria de bebida: 0 - beer, 1 - spirit, 2 - wine, 3 - none '''
-nova_coluna_numerica = []
+'''Atenção¹: nessa nova coluna atribuirei valores numéricos/nominais para cada categoria de bebida: beer, none, spirit, wine.'''
 nova_coluna_nominal = []
 
 for index, column in bebida_mundo.iterrows():
    if column['beer_servings'] > column['spirit_servings'] and column['beer_servings'] > column['wine_servings']:
-       nova_coluna_numerica.append(0)
        nova_coluna_nominal.append('beer')
    elif column['spirit_servings'] > column['beer_servings'] and column['spirit_servings'] > column['wine_servings']:
-       nova_coluna_numerica.append(1)
        nova_coluna_nominal.append('spirit')
    elif column['wine_servings'] > column['beer_servings'] and column['wine_servings'] > column['spirit_servings']:
-       nova_coluna_numerica.append(2)
        nova_coluna_nominal.append('wine')
    elif column['wine_servings'] == column['beer_servings'] and column['wine_servings'] == column['spirit_servings']:
-       nova_coluna_numerica.append(3)
        nova_coluna_nominal.append('none')
        
 #Adicionando as novas colunas
-bebida_mundo['most_consumed_number'] = nova_coluna_numerica
 bebida_mundo['most_consumed_nominal'] = nova_coluna_nominal
 
 '''Obs¹: Como na minha base de dados eu não tenho/selecionei variáveis categóricas, não haverá a necessidade de transformação das mesmas '''
@@ -64,6 +59,12 @@ confusao = confusion_matrix(y_teste, previsoes)
 taxa_acerto = accuracy_score(y_teste, previsoes)
 taxa_erro = 1 - taxa_acerto
 
+#Gera a figura da matriz de confusão
+v = ConfusionMatrix(arvore)
+v.fit(X_treinamento, y_treinamento)
+v.score(X_teste, y_teste)
+v.poof()
+
 '''Obs³: Para efeito de comparação, decidi ajustar o teste_size para ver quais valores seriam melhor para o teste de árvore de decisão da máquina, e o resultado foi esse:
     
          Como constatado, a taxa de acerto foi 89.6%, aproximadamente, com 30% de dados para teste com todos os 193 países.
@@ -78,5 +79,5 @@ taxa_erro = 1 - taxa_acerto
          Como constatado, a taxa de acerto foi 90.2%, aproximadamente, com 21% de dados para teste com todos os 193 países.
          Como constatado, a taxa de acerto foi 89.7%, aproximadamente, com 20% de dados para teste com todos os 193 países.
          
-         Conclusão, o melhor resultado da máquina foi um valor de 0.22(22%) dos dados para o teste e 0.78(78%) dos dados para treinamento. A conclusão que se
+         Conclusão, o melhor resultado da máquina foi um valor de 0.22(22%) dos dados para o teste e 0.88(88%) dos dados para treinamento. A conclusão que se
          tira é de que há um limite para os dados de treinamento. Nem sempre quanto mais dados para treinamento, melhor será o resultado do teste.'''
