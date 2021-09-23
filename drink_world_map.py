f4e272b6b95b4b7f47df06529e596faf94c6ad02
Aristóteles Fernandes <fernandessfae@hotmail.com>
2021-09-23 07:55:48 -0300

    Update drink_world_map.py
    Adding graph generation of alcoholic beverages in the 10 largest consuming countries

--- drink_world_map.py
+++ drink_world_map.py
@@ -32,6 +32,35 @@ def drink_graphic(dataframe: pd.DataFrame,col1: str, col2: str,
     return None
 
 
+def total_alcohol_drinks(dataset: pd.DataFrame) -> None:
+    """Generation of a graph with all drinks consumed in top 10 countries
+    
+       :param dataset: database to be used
+       :type dataset: pd.DataFrame
+    """
+    total_alcohol = dataset.nlargest(10, 'total_litres_of_pure_alcohol')
+    barWidth = 0.2
+    plt.figure(figsize = (15, 5))
+    r1 = np.arange(len(total_alcohol.iloc[:, 2]))
+    r2 = [x + barWidth for x in r1]
+    r3 = [x + barWidth for x in r2]
+    plt.bar(r1, total_alcohol.iloc[:, 1], color = 'orange',
+            width = barWidth, label = 'Beer')
+    plt.bar(r2, total_alcohol.iloc[:, 2], color = 'blue',
+            width = barWidth, label = 'Spirit')
+    plt.bar(r3, total_alcohol.iloc[:, 3], color = 'pink',
+            width = barWidth, label = 'Wine')
+    plt.xticks([r + barWidth for r in range(len(total_alcohol.iloc[:, 2]))],
+               total_alcohol['country'])
+    plt.ylabel('beer cans/spirit shots/wine glasses')
+    plt.title('The 10 countries that consume the most total alcohol per year',
+              fontdict= {'fontsize': 16, 'fontweight':'bold'})
+    plt.legend(loc = 'best')
+    plt.tight_layout()
+    plt.show()
+    return None
+
+
 url: str = 'https://raw.githubusercontent.com/fivethirtyeight/data/master'
 specific_url: str = f'{url}/alcohol-consumption/drinks.csv'
 url_datum: requests.models = requests.get(specific_url)
@@ -40,6 +69,9 @@ if url_datum.ok:
     data: str = url_datum.content.decode('utf-8')
     dataset: pd.DataFrame = pd.read_csv(io.StringIO(data))
     #print(dataset.columns)
+    
+
+if __name__ == '__main__':
     drink_graphic(dataset,'country','beer_servings',
                   'The 10 countries that consume the most beer per year')
     drink_graphic(dataset,'country','wine_servings',
@@ -48,4 +80,4 @@ if url_datum.ok:
                   'The 10 countries that consume the most spirit per year')
     drink_graphic(dataset,'country','total_litres_of_pure_alcohol',
                   'The 10 countries that consume the most total alcohol per year')
-    
+    total_alcohol_drinks(dataset)
